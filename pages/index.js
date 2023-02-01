753797909bbbbe26aeb7fb71663589690da9f534
Matej Kukolik <dalinec123@gmail.com>
2023-02-01 12:07:01 +0100

    add footer component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Footer from '@/features/components/Footer';
 import Navbar from '@/features/components/Navbar';
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
